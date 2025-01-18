f4943aac7182176fa32b3562dffe9228178e62e8
Yogurtking <yogurtking@gmail.com>
2025-01-17 22:11:52 -0800

    added drawing of the player ship, and rotation

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
+    fps_limiter = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        ship.update(dt)
         screen.fill(BLACK)
+        ship.draw(screen)
+        print(f"Delta time: {round(dt, 3)} seconds")
         pygame.display.flip()
+        delta = fps_limiter.tick(60)
+        dt =  (delta / 1000)
 
 if __name__ == "__main__":
     main()
---
+++ player.py
@@ -0,0 +1,31 @@
+from constants import *
+from circleshape import *
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    # copied from boot.dev
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    # draw the ship
+    def draw(self, screen):
+        pygame.draw.polygon(screen, WHITE, self.triangle(), 2)
+    
+    # allow the ship to rotate
+    def rotate(self, dt):
+        self.rotation += (PLAYER_TURN_SPEED * dt)
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
